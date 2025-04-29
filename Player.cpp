dca79b849707baae25531839ed4d9dca9690afde
junios-study <31557850+junios-study@users.noreply.github.com>
2025-04-29 14:43:42 +0900

    키 눌렀을때만 처리

--- Player.cpp
+++ Player.cpp
@@ -22,23 +22,26 @@ APlayer::~APlayer()
 
 void APlayer::Tick()
 {
-	switch (GEngine->Event.key.key)
+	if (GEngine->Event.type == SDL_EVENT_KEY_DOWN)
 	{
-	case SDLK_W:
-	case SDLK_UP:
-		AddActorWorldOffset(FVector2D(0, -1));
-		break;
-	case SDLK_S:
-	case SDLK_DOWN:
-		AddActorWorldOffset(FVector2D(0, 1));
-		break;
-	case SDLK_A:
-	case SDLK_LEFT:
-		AddActorWorldOffset(FVector2D(-1, 0));
-		break;
-	case SDLK_D:
-	case SDLK_RIGHT:
-		AddActorWorldOffset(FVector2D(1, 0));
-		break;
+		switch (GEngine->Event.key.key)
+		{
+		case SDLK_W:
+		case SDLK_UP:
+			AddActorWorldOffset(FVector2D(0, -1));
+			break;
+		case SDLK_S:
+		case SDLK_DOWN:
+			AddActorWorldOffset(FVector2D(0, 1));
+			break;
+		case SDLK_A:
+		case SDLK_LEFT:
+			AddActorWorldOffset(FVector2D(-1, 0));
+			break;
+		case SDLK_D:
+		case SDLK_RIGHT:
+			AddActorWorldOffset(FVector2D(1, 0));
+			break;
+		}
 	}
 }
